a36173e76efcd28e964c1f3d1f7e45639927cc2d
Ulises Cabrera <ucabrera@outlook.com>
2016-01-16 07:29:04 +0000

    added patterns and download bottom

--- public/js/fullStack/services/fullStack.service.js
+++ public/js/fullStack/services/fullStack.service.js
@@ -5,6 +5,19 @@ angular.module("FullStackModule")
             projects :[
                 {
                     name: "Test 1",
+                    description: "Hello from Test 1",
+                    image: {"background-image" : "url(' images/angular-3.jpg')"}
+                },
+                {
+                    name: "Test 3",
+                    description: "Hello from Test 3"
+                },                
+                {
+                    name: "Test 2",
+                    description: "Hello from Test 2"
+                },
+                {
+                    name: "Test 1",
                     description: "Hello from Test 1"
                 },
                 {
@@ -14,7 +27,7 @@ angular.module("FullStackModule")
                 {
                     name: "Test 2",
                     description: "Hello from Test 2"
-                }
+                }                
         ]};
         
         return projectsFactory;
