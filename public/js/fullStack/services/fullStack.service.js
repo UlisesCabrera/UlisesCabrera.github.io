6e4e5a206919e6b011eb44ce4b22ad98f787b5be
Ulises Cabrera <ucabrera@outlook.com>
2016-01-16 05:50:32 +0000

    styled tabs and cards

--- public/js/fullStack/services/fullStack.service.js
+++ public/js/fullStack/services/fullStack.service.js
@@ -8,6 +8,10 @@ angular.module("FullStackModule")
                     description: "Hello from Test 1"
                 },
                 {
+                    name: "Test 3",
+                    description: "Hello from Test 3"
+                },                
+                {
                     name: "Test 2",
                     description: "Hello from Test 2"
                 }
--- public/js/homePage/controllers/homePage.controller.js
+++ public/js/homePage/controllers/homePage.controller.js
@@ -1,4 +1,28 @@
 angular.module("HomePageModule")
-    .controller("HomePageController",["$scope", function($scope){
+    .controller("HomePageController",["$scope", "$location", function($scope, $location){
+        
+        // determine current location
+        $scope.current = function() {
+            var location;
+            switch ($location.path()) {
+                case '/':
+                    location = 'home';
+                    break;
+                case '/fullStack':
+                    location = 'fullStack';
+                    break;
+                case '/front-end':
+                    location = 'front-end';
+                    break;
+                case '/microservice':
+                    location = 'microservice';
+                    break;                    
+                default:
+                    location = 'home';
+            }
+        
+            return location;
+        };
+        
         $scope.appName = "Ulises Cabrera";
 }]);
