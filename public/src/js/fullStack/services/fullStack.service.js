029eef157d8a0b6bffbdca560123ffc909a0fef5
Ulises Cabrera <ucabrera@outlook.com>
2016-02-21 16:13:04 +0000

    added stock watcher project

--- public/src/js/fullStack/services/fullStack.service.js
+++ public/src/js/fullStack/services/fullStack.service.js
@@ -4,6 +4,20 @@ angular.module("FullStackModule")
         var projectsFactory = { 
             projects :[
                 {
+                    name: "Chart the Stock Market App",
+                    description:"It's an app built to track market stocks, it uses socket.io to sync all clients connected built with the MEAN stack.  It lets the user add, remove and update stocks from the chart, any changes to the chart is view on every client connected. The data is coming from the Quandl financial API and the chart is built with the highchart library.",
+                    image: {"background-image" : "url(' images/fullStackProjects/stock-watcher-project.jpg')"},
+                    source: 'https://github.com/UlisesCabrera/the-stock-market-charts',
+                    live: 'https://stock-w.herokuapp.com',
+                    post:'http://www.ulisescabrera.com/blog/2016/02/21/building-chart-the-stock-market/',
+                    userStories:['I can view a graph displaying the recent trend lines for each added stock.',
+                    'I can add new stocks by their symbol name.',
+                    'I can remove stocks.',
+                    'I can see changes in real-time when any other user adds or removes a stock.'],
+                    school: 'freeCodeCamp',
+                    date: 'February 2016'
+                },                
+                {
                     name: "Nightlife Coordination App",
                     description: "It's an app to help users find the local bars using their current location or by searching on the map, it lets them add themselves to a list of 'People going'  and they can also remove themselves from the list. Keeping track of how many people are going to each bar will help the users decide where they want to go tonight. The app was built with the MEAN stack with the combination of the Yelp API to get the all the info for the local bars and using Google maps to display the location of each bar.",
                     image: {"background-image" : "url(' images/fullStackProjects/night-life-coord-project.jpg')"},
