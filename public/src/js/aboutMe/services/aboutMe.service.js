0d12b70f1828f9eb629bc5a78aca5775f1f6df8b
Ulises Cabrera <ucabrera@outlook.com>
2016-02-23 16:21:33 +0000

    updated resume

--- public/src/js/aboutMe/services/aboutMe.service.js
+++ public/src/js/aboutMe/services/aboutMe.service.js
@@ -8,7 +8,7 @@ angular.module('AboutMeModule')
                 aboutMe: "I am a motivated front-end developer with knowledge in Javascript, Jquery, bootstrap and the MEAN stack. I enjoy building new stuff and pushing my skills to their limits. I am hardworking and a fast learner with good communication skills and a strong design background.",
                 projects: { 
                     projectsName: ['Front-End', 'Full Stack', 'Microservices'],
-                    projectsCount:[14 , 2, 5]
+                    projectsCount:[14 , 3, 5]
                 },
                 skills: [
                     {
@@ -62,7 +62,7 @@ angular.module('AboutMeModule')
             // getting random colors
             var skillsColors = randomColor({ count: aboutMeInfo.skills.length, luminosity: 'dark'});
             
-            //pushing adding colors to skill objects
+            //pushing colors to skill objects
             skillsColors.forEach(function(color, idx){
                 aboutMeInfo.skills[idx].styleCSS = {"border" : "1px solid "+ color, "color" : color};
             });
